f5cefe16bd338c943ab83c7e615cc08677468da9
Akhmed A <ahmed071998@gmail.com>
2018-11-29 11:49:09 +0100

    Some stuff has been added to Character class

--- src/Text_Adventure/Characters/Character.java
+++ src/Text_Adventure/Characters/Character.java
@@ -1,26 +1,47 @@
 package Text_Adventure.Characters;
 
+import java.util.Random;
+
 public class Character {
     private String name;
     private int roomIndex;
+    private double hitChance;
+    private int maxAttack;
 
     //Set of constructors that are defined through setters (which show up later)
     public Character() {
-        this(null, -1);
+        this(null, -1, 0.5, 50);
     }
     public Character(String name) {
-        this(name, -1);
+        this(name, -1, 0.5, 50);
     }
-    public Character(String name, int index) {
+    public Character(String name, int index, double hitChance) { this(name, -1, hitChance, 50); }
+    public Character(String name, int index, double hitChance, int attack) {
         setName(name);
         setRoomIndex(index);
+        setHitChance(hitChance);
+        setMaxAttack(attack);
     }
 
     //setters for name and roomIndex
     public void setName(String name) { this.name = name; }
     public void setRoomIndex(int index) { roomIndex = index; }
+    public void setHitChance(double chance) { hitChance = chance; }
+    public void setMaxAttack(int attack) { maxAttack = attack; }
 
     //getters for name and roomIndex
     public String getName() { return name; }
     public int getRoomIndex() { return roomIndex; }
+    public double getHitChance() { return hitChance; }
+    public int getMaxAttack() { return maxAttack; }
+
+    public int doAttack() {
+        Random number = new Random();
+        return (hitChance > number.nextDouble()) ? maxAttack - number.nextInt(maxAttack): 0;
+    }
+
+
+
+    public static void main(String[] args) {
+    }
 }
--- src/Text_Adventure/Main.java
+++ src/Text_Adventure/Main.java
@@ -1,4 +1,5 @@
 package Text_Adventure;
 
 public class Main {
+
 }

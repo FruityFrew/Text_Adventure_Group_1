eaad2c3ddad277614856f0e38bf7d51ad82a4c5f
Robert Kristian Alm <exarchiasghost@gmail.com>
2018-12-01 14:40:31 +0100

    Done for now with the descriptors
    I have to at least add the comments before I merge it.
    I will have to add some spawners and some helping functions as well.

--- src/Text_Adventure/Room.java
+++ src/Text_Adventure/Room.java
@@ -19,6 +19,10 @@ public class Room {
     public Room(int x, int y){
         this.position[0] = x;
         this.position[1] = y;
+        this.doorPosition[0] = random.nextBoolean();
+        this.doorPosition[1] = random.nextBoolean();
+        this.doorPosition[2] = random.nextBoolean();
+        this.doorPosition[3] = random.nextBoolean();
     }
 
     //, boolean N, boolean E, boolean S, boolean w
@@ -35,12 +39,20 @@ public class Room {
         this.position[0] = x;
         this.position[1] = y;
         this.item = item;
+        this.doorPosition[0] = random.nextBoolean();
+        this.doorPosition[1] = random.nextBoolean();
+        this.doorPosition[2] = random.nextBoolean();
+        this.doorPosition[3] = random.nextBoolean();
     }
 
     public Room(int x, int y, Monster monster){
         this.position[0] = x;
         this.position[1] = y;
         this.monster = monster;
+        this.doorPosition[0] = random.nextBoolean();
+        this.doorPosition[1] = random.nextBoolean();
+        this.doorPosition[2] = random.nextBoolean();
+        this.doorPosition[3] = random.nextBoolean();
     }
 
     public Room(int x, int y, Item item, Monster monster){
@@ -48,6 +60,10 @@ public class Room {
         this.position[1] = y;
         this.item = item;
         this.monster = monster;
+        this.doorPosition[0] = random.nextBoolean();
+        this.doorPosition[1] = random.nextBoolean();
+        this.doorPosition[2] = random.nextBoolean();
+        this.doorPosition[3] = random.nextBoolean();
     }
 
     public Room(int x, int y, Monster monster, Item item){
@@ -66,6 +82,52 @@ public class Room {
         this.doorPosition[3] = W;
         this.monster = monster;
         this.item = item;
+    }
+
+    public void describeRoom(){
+        System.out.println("You entering the room as you are looking around carefully...");
+        describeWalls();
+        describeDoors(doorPosition[0], doorPosition[1], doorPosition[2], doorPosition[3]);
+        describeItem();
+        describeMonster();
+    }
+
+    public void describeWalls(){
+        System.out.println("The walls are looking like walls right now.");
+    }
+
+    public void describeDoors(boolean N, boolean E, boolean S, boolean W){
+        System.out.println("you are looking out to find out how many doors they are...");
+        if (N){
+            System.out.println("One door stands on the North side of the room");
+        }if (E){
+            System.out.println("you can see one door on the East side of the room");
+        }if (N){
+            System.out.println("It seems that there is a room on the South side of the room");
+        }if (N){
+            System.out.println("and one door stands on the West side of the room as well");
+        }
+    }
+
+    public void describeItem(){
+        System.out.println("You are looking for items...");
+        if(this.item != null){
+            System.out.println("In the corner seems to be an item,");
+            System.out.println("It is a " + this.item.getName() + "!");
+        } else {
+            System.out.println("you didn't found something useful.");
+        }
+    }
+
+    public void describeMonster(){
+        System.out.println("You are checking if someone is in the room...");
+        if(this.monster != null){
+            System.out.println("In the shadows you see a figure.");
+            System.out.println("Oh no! It is a Monster!");
+            //System.out.println("Oh no! It is a " + this.monster.getType() + "!");
+        } else {
+            System.out.println("No one. Probably just the wind");
+        }
     }
 
     public void setPosition(int[] position) {

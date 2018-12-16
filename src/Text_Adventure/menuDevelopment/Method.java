10a4e7e7059bb1506a5e0404e5cf962434df972c
Robert Kristian Alm <exarchiasghost@gmail.com>
2018-12-16 02:42:30 +0100

    No more infinitive items
    now the room remembers when the players takes an item.

--- src/Text_Adventure/menuDevelopment/Method.java
+++ src/Text_Adventure/menuDevelopment/Method.java
@@ -486,10 +486,11 @@ public class Method {
                                     //        "\nYou just drunk this black coffee... hm, if you can call this dark liquid ..'coffee'!\n" +
                                     //        "---------------------------------------------------------------------------------------\n");
 
-                                    //robert: I temporarely disabled this line bellow me.
+                                    //robert: I disabled this line bellow me.
                                     //hero1.addItemToBackpack(room1.getItem());
                                     //robert: I added this line bellow me.
                                     hero1.pickItem(room1.getItem());
+                                    room1.setItem(null);
                                     //Main.playerHealth += 50;// the Consumable Class is irrelevant in this form, it needs to be completed with different types of items, as we decided.
                                     // Coffee = 50 health points.
                                     System.out.println("\n----------------------------------------\n\tYour health is now " + Main.playerHealth + "." +

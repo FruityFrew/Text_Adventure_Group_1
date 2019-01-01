de1cf0db4a997fa4de8a486b140190be40274851
Nemanja Negovanovic <nexy2307@hotmail.rs>
2019-01-01 22:32:31 +0100

    added map open/close sound

--- src/Text_Adventure/menuDevelopment/Sound_methods.java
+++ src/Text_Adventure/menuDevelopment/Sound_methods.java
@@ -7,28 +7,28 @@ import java.util.concurrent.Executors;
 public class Sound_methods implements Serializable {
 
     public static Play_Sound soundFX = new Play_Sound();
+    public ExecutorService Thread = Executors.newFixedThreadPool(4);
+    public ExecutorService Threads = Executors.newFixedThreadPool(4);
     public void kickSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/kick.wav"); }});
     }
     public void missSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/miss.wav"); }});
     }
     public void winSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/gameWon.wav"); }});
     }
     public void loseSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/loseGame.wav"); }});
     }
     public void treasureSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/treasure.wav"); }});
     }
     public void playerDeathSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/playerDeath.wav"); }});
     }
     public void monsterDeathSound(){
@@ -36,50 +36,52 @@ public class Sound_methods implements Serializable {
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/monsterDeath.wav"); }});
     }
     public void laughSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/laugh.wav"); }});
     }
     public void backpackSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/openBackPack.wav"); }});
     }
     public void drinkPotionSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/drinkPotion.wav"); }});
     }
     public void dropPotionSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/dropItem.wav"); }});
     }
     public void walkSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/walking.wav"); }});
         ASCII.walking();
         Method.clearScreen();}
     public void menuSound(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/click.wav"); }});
     }
     public void pickUp(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/pickUp.wav"); }});
     }
     public void avoidFight(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/flee.wav"); }});
     }
     public void doorLocked(){
-        ExecutorService Thread = Executors.newFixedThreadPool(4);
+
         Thread.submit(new Runnable() {public void run() { soundFX.playSound("sounds/locked.wav"); }});
     }
     public void unlockingDoor(){
-        ExecutorService Threads = Executors.newFixedThreadPool(4);
         Threads.submit(new Runnable() {public void run() { soundFX.playSound("sounds/unlocking.wav"); }});
         try {
             Thread.sleep(1500);
         }catch (Exception e){
             System.out.println("Could not perform Thread.sleep method!");
         }
+    }
+    public void mapSound(){
+        Threads.submit(new Runnable() {public void run() { soundFX.playSound("sounds/map.wav"); }});
     }
 
 }
